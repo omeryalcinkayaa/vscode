fe86e27095a25ba55d0da29517503297d2b46c25
Ömer Yalçınkaya <59436916+omeryalcinkayaa@users.noreply.github.com>
2022-04-25 17:36:12 +0300

    Add files via upload

---
+++ cs1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+namespace cs
+{
+    public class cs1
+    {        String inputString;     
+        inputString = Console.ReadLine(); 
+        Console.WriteLine("Hello, World.");        
+        Console.WriteLine(inputString);
+    }
+}
