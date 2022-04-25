436e84c7efecae9eb0e78f70394be6cbf63529ac
Ömer Yalçınkaya <59436916+omeryalcinkayaa@users.noreply.github.com>
2022-04-25 17:49:36 +0300

    Add files via upload

---
+++ operatörler.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace  
+{
+    public class operatörler
+    {
+        int a=1;
+        int b=2;
+        if (a==b) Console.WriteLine("Eşit")
+        else if(a<b)Console.WriteLine("B büyük");
+        else if(a>b) Console.WriteLine("A büyük") 
+           }
+}
